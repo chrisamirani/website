b1b942be3caccf325c56a5ab35c01985932d4588
chrisamirani <chrisamirani@yahoo.com>
2021-01-09 11:13:04 +0800

    react nav and favicons

--- home_files/custom.js
+++ home_files/custom.js
@@ -2,7 +2,7 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+class EmailButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = { liked: false };
@@ -15,11 +15,39 @@ class LikeButton extends React.Component {
 
     return e(
       'a',
-      { onClick: () => this.setState({ liked: true }), class:"btn btn-lg btn-secondary fw-bold border-white bg-white"},
+      { onClick: () => this.setState({ liked: true }), className:"btn btn-lg btn-secondary fw-bold border-white bg-white"},
       'Show Email',
     );
   }
 }
 
+class NavOptions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { home: "nav-link active",skills:"nav-link",projects:"nav-link" };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return e('a',{href:"#"},"chrisamirani@yahoo.com")
+    }
+
+    return e(
+      "nav",
+      {className:"nav nav-masthead justify-content-center float-md-end"},
+      e('a',
+      { onClick: () => this.setState({ home: "nav-link active",skills:"nav-link",projects:"nav-link" }), className:this.state.home},
+      'Home'),
+      e('a',
+      { onClick: () => this.setState({ home: "nav-link",skills:"nav-link active",projects:"nav-link" }), className:this.state.skills},
+      'Skills'),
+      e('a',
+      { onClick: () => this.setState({ home: "nav-link",skills:"nav-link",projects:"nav-link active" }), className:this.state.projects},
+      'Projects')
+    );
+  }
+}
+const navContainer = document.querySelector('#nav');
+ReactDOM.render(e(NavOptions), navContainer);
 const domContainer = document.querySelector('#email');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(EmailButton), domContainer);
