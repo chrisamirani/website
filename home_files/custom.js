f71b276b82a8419fb53605654a345234320a4717
chrisamirani <chrisamirani@yahoo.com>
2021-01-11 21:34:52 +0800

    pages updated

--- home_files/custom.js
+++ home_files/custom.js
@@ -1,53 +1,29 @@
-'use strict';
-
-const e = React.createElement;
-
-class EmailButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return e('a',{href:"#"},"chrisamirani@yahoo.com")
+let current = document.getElementById('1');
+let content = document.getElementById("body")
+function activate(el) {
+    if (current != null)
+     {
+         current.className = "nav-link";
+     }
+     el.className = "nav-link active";
+     current = el;
+     if(el.id == 1){
+         content.innerHTML = `<h1 class="ml11">
+         <span class="text-wrapper">
+           <span class="line line1"></span>
+           <span class="letters">M. Chehelamirani</span>
+         </span>
+       </h1>
+       <p class="lead">Full Stack Developer</p>
+       <p class="lead" id="content">
+         
+       </p>`
+     }
+     if(el.id == 2){
+         content.innerHTML = "2"
+     }
+     if(el.id == 3){
+        content.innerHTML = "3"
     }
 
-    return e(
-      'a',
-      { onClick: () => this.setState({ liked: true }), className:"btn btn-lg btn-secondary fw-bold border-white bg-white"},
-      'Show Email',
-    );
-  }
 }
-
-class NavOptions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { home: "nav-link active",skills:"nav-link",projects:"nav-link" };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return e('a',{href:"#"},"chrisamirani@yahoo.com")
-    }
-
-    return e(
-      "nav",
-      {className:"nav nav-masthead justify-content-center float-md-end"},
-      e('a',
-      { onClick: () => this.setState({ home: "nav-link active",skills:"nav-link",projects:"nav-link" }), className:this.state.home},
-      'Home'),
-      e('a',
-      { onClick: () => this.setState({ home: "nav-link",skills:"nav-link active",projects:"nav-link" }), className:this.state.skills},
-      'Skills'),
-      e('a',
-      { onClick: () => this.setState({ home: "nav-link",skills:"nav-link",projects:"nav-link active" }), className:this.state.projects},
-      'Projects')
-    );
-  }
-}
-const navContainer = document.querySelector('#nav');
-ReactDOM.render(e(NavOptions), navContainer);
-const domContainer = document.querySelector('#email');
-ReactDOM.render(e(EmailButton), domContainer);
