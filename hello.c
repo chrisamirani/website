c1d72da66b2007278194ee5a67ed9f74d887ae15
chrisamirani <chrisamirani@yahoo.com>
2021-03-02 12:52:54 +0300

    hello world

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!\n");
+    return 0;
+}
+
+/*
+Notes:
+when you compile a program and want to run it, do so directly from the console. If 
+you double click on it, it might run then exit.
+*/
